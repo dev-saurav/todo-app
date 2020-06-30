9cba8dd82581bb267f75681038ad17267e98a06f
webDevSaurav <webdevelopersaurav@gmail.com>
2020-06-30 23:45:42 +0530

    Updates

---
+++ precache-manifest.04dc57e51590f6262b2e418fabb95025.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2ad30fbc8af315fff810ecf972403b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "cee037f75a97e11ae87e",
+    "url": "/todo-app/static/js/2.4cea7b68.chunk.js"
+  },
+  {
+    "revision": "91f2627fd20a717bf8f5",
+    "url": "/todo-app/static/js/main.54d8519d.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
